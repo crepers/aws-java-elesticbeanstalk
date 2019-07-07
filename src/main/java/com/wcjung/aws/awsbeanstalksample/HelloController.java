75139c2a12b736f32437c7246612a7a0f5742216
crepers12 <woochuljung@woochului-MacBook-Air.local>
2019-07-07 22:08:40 +0900

    Create sample project.

---
+++ src/main/java/com/wcjung/aws/awsbeanstalksample/HelloController.java
@@ -0,0 +1,14 @@
+package com.wcjung.aws.awsbeanstalksample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+	@GetMapping
+	public String defaultPage() {
+		return "Hello World!!!";
+	}
+}
